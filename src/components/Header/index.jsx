1804a7073f8743206a2a4c7b70287441dfa5924d
IvanVulovic <ivn.vulovic@gmail.com>
2022-10-09 20:59:49 +0200

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,8 @@
+import Logo from "../Logo";
+import "./style.scss";
+
+export default function Header(){
+    return <header className="header">
+        <Logo />
+    </header>
+}
---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import './style.scss';
+
+export default function Logo(){
+    return <Link to="/" className="logo">
+        <div>
+            <h1>Teambuildingz</h1>
+            <h2>more than analytics</h2>
+        </div>
+    </Link>
+}
